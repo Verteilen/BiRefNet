48e65ae27999f0b8669e0598475cd26d4886fdf3
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-08-04 14:55:12 +0800

    Update

--- test.py
+++ test.py
@@ -103,7 +103,7 @@ for image_path in image_paths[:]:
         array_background[:, :, :] = (0, 0, 0)
         array_foreground_background = (array_foreground * array_mask + array_background * (1 - array_mask)).astype(np.uint8)
         com_img = Image.new('RGB', (image.width, image.height))
-        Image = Image.filter(lut)
+        image = image.filter(lut)
         com_img.paste(Image.fromarray(array_foreground_background), (0, 0))
         com_img.save(image_path.replace(src_dir, final_dir))
 
